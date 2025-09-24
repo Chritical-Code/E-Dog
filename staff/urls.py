d6e613c10b9c414844b0328193ff5312c4bf221c
purpleviking <purpleviking221@gmail.com>
2025-09-24 16:19:56 -0400

    Unapproved posts now link to a custom page, which will soon have controls.

--- staff/urls.py
+++ staff/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("approval/", views.approval),
+    path("approval/", views.approval, name="approval"),
+    path("review/<int:postID>/", views.review, name="review"),
 ]
--- staff/views.py
+++ staff/views.py
@@ -1,14 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from post.postBox import PostBox
+from post.models import Post, Image
+from django.db.models import Q
 
 @login_required
 def index(request):
-    context = {}
+    #staff only
+    if(request.user.is_staff != True):
+        return redirect("/browse/")
     
+    context = {}
     return render(request, "staff/index.html", context)
 
 @login_required
 def approval(request):
-    context = {}
+    #staff only
+    if(request.user.is_staff != True):
+        return redirect("/browse/")
 
+    #retrieve recent posts
+    unapprovedPosts = (
+        Post.objects.filter(image__isnull=False).distinct() #has images
+        .filter(approved__isnull=True).distinct() #unapproved
+        .exclude(breeds="").order_by("dateCreated")[:10] #has breed, oldest first, stop at 10
+    )
+    
+    linkType = "/staff/review/"
+    unapprovedPostBoxes = PostBox.easyCombine(unapprovedPosts, linkType)
+
+    context = {
+        "unapprovedPostBoxes": unapprovedPostBoxes
+    }
     return render(request, "staff/approval.html", context)
+
+@login_required
+def review(request, postID):
+    #staff only
+    if(request.user.is_staff != True):
+        return redirect("/browse/")
+    
+    post = get_object_or_404(Post, pk=postID)
+    allImages = Image.objects.filter(post=postID)
+
+    context = {
+        "post": post,
+        "allImages": allImages,
+    }
+
+    return render(request, "staff/review.html", context)
