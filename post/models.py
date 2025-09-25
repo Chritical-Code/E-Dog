75e1797b9590a4d56c0f5f5f68b460550bbcc22f
purpleviking <purpleviking221@gmail.com>
2025-09-25 14:19:43 -0400

    Uploaded file names are now randomized.

--- post/models.py
+++ post/models.py
@@ -1,4 +1,7 @@
 #import
+import datetime
+import os
+from uuid import uuid4
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
@@ -39,7 +42,14 @@ def validate_image_size(image):
     if image.size > 8 * 1024 * 1024:
         raise ValidationError("Image file too large (8mb)")
 
+#generate a random file name
+def custom_upload_to(instance, filename):
+    ext = filename.split('.')[-1]
+    date_path = datetime.date.today().strftime('%Y/%m/%d')
+    new_filename = f"{uuid4().hex}.{ext}"
+    return os.path.join('pics', date_path, new_filename)
+
 class Image(models.Model):
-    photo = models.ImageField(upload_to="pics/%Y/%m/%d/", validators=[validate_image_size])
+    photo = models.ImageField(upload_to=custom_upload_to, validators=[validate_image_size])
     title = models.CharField(max_length=20)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
